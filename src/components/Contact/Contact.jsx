94bcfa01ae0ffdafea2f20de87f030e1af918675
Frida Mattsson <frida_mattsson97@hotmail.com>
2024-08-06 17:43:49 +0800

    add social icons to contact and amend issue with hero image

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -11,10 +11,16 @@ export const Contact = () => {
                 <li className={styles.contactItem}>
                     <div className={styles.contactItemText}>
                         <h3>Want to get in touch?</h3>
-                        <p>Feel free to send me an email, give me a call or message me on LinkedIn. I'll happily get back to you ASAP.</p>
+                        <p>Feel free to email me on <a href="mailto:frida_mattsson97@hotmail.com" className={styles.email}>frida_mattsson97@hotmail.com</a>, give me a call or message me on LinkedIn. I'll happily get back to you ASAP.</p>
                     </div>
                 </li>
             </ul>
         </div>
+        <a href="https://www.linkedin.com/in/fridamattsson/" target="_blank" rel="noopener noreferrer">
+            <img src={getImageUrl("contact/linkedin.png")} alt="linkedin logo" className={styles.socialIcon} />
+        </a>
+        <a href="https://wa.me/86695306" target="_blank" rel="noopener noreferrer">
+            <img src={getImageUrl("contact/whatsapp.png")} alt="whatsapp logo" className={styles.socialIcon} />
+        </a>
     </section>;
 }
